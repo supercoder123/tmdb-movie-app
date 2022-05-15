2151ea17fdaf02dbcb86fa07fc6d4c94212cb732
ashley.fernandes <ashley.fernandes@upstox.com>
2022-05-15 20:47:18 +0530

    added correct hover animation

--- src/components/MoviePosterCard/MoviePosterCard.tsx
+++ src/components/MoviePosterCard/MoviePosterCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from '@/components/Link';
+import { Link } from '@/components/Link';
 import { Text } from '@/components/Text';
 import { motion } from 'framer-motion';
 
@@ -10,18 +10,18 @@ type MoviePosterCardProps = {
 	movieId: number;
 }
 
-const MoviePosterCardWrapper = styled.div<{imgUrl: string}>`
+const MoviePosterCardWrapper = styled(motion.div)<{imgUrl: string}>`
     border-radius: 8px;
     height: 185px;
     width: 126px;
 	margin: 20px 10px;
     flex-direction: column;
-	background: linear-gradient(0deg,rgb(14 14 14),rgba(24,22,24,0.3)), url(${(props) => props.imgUrl});
+    display: flex;
+	overflow: hidden;
+	background: url(${(props) => props.imgUrl});
 	background-position: top center;
 	background-repeat: no-repeat;
 	background-size: cover;
-    display: flex;
-	overflow: hidden;
 `;
 
 const TextWrapper = styled(motion.div)`
@@ -29,14 +29,15 @@ const TextWrapper = styled(motion.div)`
 	height: 100%;
 	width: 100%;
 	display: flex;
+	background: linear-gradient(0deg, rgba(2,0,36,0.9) 0%, rgba(2,0,36,0.5) 38%, rgba(0,0,0,0.029) 100%);
 `;
 
 const MoviePosterCardWithoutMemo = ({movieId, title, imgUrl}: MoviePosterCardProps) => {
 	return (
 		<Link to={`/movies/${movieId}`}>
 			<MoviePosterCardWrapper imgUrl={imgUrl}>
-				<TextWrapper initial={{y: -20}} whileHover={{opacity: 1, y: 0}}>
-					<Text fontWeight="bold" mt={'auto'} mx={10} marginBottom={10}>{title}</Text>
+				<TextWrapper initial={{y: 20}} whileHover={{opacity: 1, y: 0}} transition={{easings: 'linear', duration: 0.3}}>
+					<Text fontWeight="bold" fontSize={18} mt={'auto'} mx={10} marginBottom={10}>{title}</Text>
 				</TextWrapper>
 			</MoviePosterCardWrapper>
 		</Link>
